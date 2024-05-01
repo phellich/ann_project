1e5d25e537839f94b6e6810f367f236b186a2c9b
hellich <pierre.hellich@epfl.ch>
2024-05-01 17:29:29 +0200

    manque juste update function pour le 3.1 et 3.2

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import torch
 
 def epsilon_greedy(env, epsilon, Q):
     """
@@ -10,16 +10,10 @@ def epsilon_greedy(env, epsilon, Q):
     :return:
         - the chosen action
     """
-    # get the available positions
-    available_actions = env.available()
 
     if np.random.uniform(0, 1) < epsilon:
         # with probability epsilon make a random move (exploration)
-        return str(np.random.choice(available_actions))
+        return env.action_space.sample()
     else:
         # with probability 1-epsilon choose the action with the highest immediate reward (exploitation)
-        q = np.copy(Q[env.get_state()])
-        mask = [env.encode_action(action) for action in available_actions]
-        q = [q[i] if i in mask else np.nan for i in range(len(q))]
-        max_indices = np.argwhere(q == np.nanmax(q)).flatten()  # best action(s) along the available ones
-        return env.inverse_encoding(int(np.random.choice(max_indices)))  # ties are split randomly
+        return torch.argmax(Q)
